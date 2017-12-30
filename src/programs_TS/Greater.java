b5a3346c9f57761e87d6c8ccbb4671c5648fda04
mounishmouny <mouni.gudigundla@gmail.com>
2017-12-30 22:06:09 +0530

    added greater code

--- src/programs_TS/Greater.java
+++ src/programs_TS/Greater.java
@@ -6,8 +6,22 @@ public class Greater {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int a=2,b=8,c=1;
+		
+		if(a>b && a>c)
+		{
+			System.out.println("A is greater");
+			
+		}
+		else if (b>a && b>c)
+		{
+			System.out.println("B is greater");
+		}
+		else
+			System.out.println("C is greater");
+			
 	}
+	
 
 }
